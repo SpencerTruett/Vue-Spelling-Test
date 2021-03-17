0fa458c05536203af23126a803fffc4ee418651c
Spencer Truett <srtruett1993@gmail.com>
2021-03-17 10:49:34 -0500

    added an input; showing up on screen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
-<template></template>
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" spellcheck="false" v-model="userInput" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
 
 <script>
-export default {
+import data from "./data";
 
+export default {
+  data() {
+    return {
+      questions: data,
+      activeIndex: 0,
+      userInput: ""
+    };
+  }
 }
 </script>
 
