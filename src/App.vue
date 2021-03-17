368ef657f084c460c6d19eb18c3a0985e3c17e04
Spencer Truett <srtruett1993@gmail.com>
2021-03-17 11:13:53 -0500

    hooked up Speech to app; IT SPEAKS!

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <input type="text" spellcheck="false" v-model="userInput" />
-    <button type="submit">Submit</button>
-  </form>
+  <div>
+    <Speech :word="questions[activeIndex].word" />
+    <form @submit.prevent="handleSubmit">
+      <input type="text" spellcheck="false" v-model="userInput" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 import data from "./data";
+import Speech from "./components/Speech"
 
 export default {
+  components: {
+    Speech
+  },
   data() {
     return {
       questions: data,
