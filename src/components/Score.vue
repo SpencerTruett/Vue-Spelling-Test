2ff6009670b910563d82271819457d1083771a99
Spencer Truett <srtruett1993@gmail.com>
2021-03-17 11:29:56 -0500

    added Score component

---
+++ src/components/Score.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Results: {{ correctCount }} / {{ data.length }}</p>
+</template>
+
+<script>
+export default {
+  props: ["questions"],
+  computed:{
+    correctCount(){
+        return this.data.filter(q => {
+        return q.word.toLowerCase() === q.userInput.toLowerCase();
+      }).length;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
